8a04fe6bc03f034a5d787e690e3b3bdbd37f610d
Roneyjrr <roneydaquila@gmail.com>
2024-10-15 16:28:14 -0300

    Comando echo

---
+++ 01-hello-word.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com PHP</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+
+    <?php
+    echo "Olá";
+    echo "<p>Programador Web - Senac Penha</p>";
+    echo "<p style='color:red'>Programador Web - Senac Penha</p>";
+
+    ?>
+
+</body>
+</html>
